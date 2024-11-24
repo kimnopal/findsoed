93af792d7577329b20879fb787ebd613ae86fc2b
kimnopal <naufalhakim366@gmail.com>
2024-11-24 22:10:30 +0700

    fix(navbar): fix login button on mobile screen

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,6 +1,6 @@
 {{-- active text-blue-600 focus:outline-none focus:text-blue-600 dark:text-blue-500 dark:focus:text-blue-500 --}}
 
-<a {{ $attributes }}
-    class="p-2 flex items-center text-sm text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500">
+<a
+    {{ $attributes->merge(['class' => 'p-2 flex items-center text-sm text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500']) }}>
     {{ $slot }}
 </a>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -240,7 +240,8 @@
 
                             </div>
                         @else
-                            <x-nav-link href="/login">
+                            <x-nav-link href="/login"
+                                class="w-full rounded-lg justify-center border border-gray-800 hover:border-gray-500">
                                 <svg class="shrink-0 size-4 me-3 md:me-2" xmlns="http://www.w3.org/2000/svg"
                                     width="24" height="24" viewBox="0 0 24 24" fill="none"
                                     stroke="currentColor" stroke-width="2" stroke-linecap="round"
