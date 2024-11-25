5dca63853673f84fb228f7dd413b495f988a3d04
kimnopal <naufalhakim366@gmail.com>
2024-11-25 11:38:50 +0700

    fix(navbar): change login button style

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -241,7 +241,7 @@
                             </div>
                         @else
                             <x-nav-link href="/login"
-                                class="w-full rounded-lg justify-center border border-gray-800 hover:border-gray-500">
+                                class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
                                 <svg class="shrink-0 size-4 me-3 md:me-2" xmlns="http://www.w3.org/2000/svg"
                                     width="24" height="24" viewBox="0 0 24 24" fill="none"
                                     stroke="currentColor" stroke-width="2" stroke-linecap="round"
